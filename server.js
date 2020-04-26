1dcf88e9c2360b52fb9179c7365825fa032dc9a1
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-04-26 15:45:12 +0800

    [refactor] Sort /cases and /doh-data-drop by date of announcement

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const apicache = require('apicache');
 const path = require('path');
 const Scraper = require('./services/Scraper');
 const Checkpoint = require('./services/Checkpoint');
-const paginate = require('paginate-array');
 
 const app = express();
 const cache = apicache.middleware;
@@ -18,170 +17,128 @@ app.use(cors());
 app.use(cache('5 hours'));
 
 app.get('/', (_, res) => {
-	res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/README.md', (_, res) => {
-	res.sendFile(path.join(__dirname + '/README.md'));
+  res.sendFile(path.join(__dirname + '/README.md'));
 });
 
 app.get('/total', async (req, res) => {
-	try {
-		const data = await scrape.getTotalCases();
-		return res.json({
-			success: true,
-			source: 'https://tiny.cc/n8nsmz',
-			data,
-		});
-	} catch (e) {
-		return res.json({
-			sucess: false,
-			message: e.message,
-		});
-	}
+  try {
+    const data = await scrape.getTotalCases();
+    return res.json({
+      success: true,
+      source: 'https://tiny.cc/n8nsmz',
+      data,
+    });
+  } catch (e) {
+    return res.json({
+      sucess: false,
+      message: e.message,
+    });
+  }
 });
 
 app.get('/cases', async (req, res) => {
-	const { page, itemsPerPage } = req.query;
-
-	try {
-		const data = await scrape.getCases();
-
-		if (page) {
-			if (isNaN(page) || isNaN(itemsPerPage)) {
-				const paginateCollection = paginate(data, 1, 100);
-				return res.json({
-					success: true,
-					source: 'https://tiny.cc/n8nsmz',
-					...paginateCollection,
-				});
-			}
-
-			const paginateCollection = paginate(data, page, itemsPerPage);
-			return res.json({
-				success: true,
-				source: 'https://tiny.cc/n8nsmz',
-				...paginateCollection,
-			});
-		}
-
-		return res.json({
-			success: true,
-			source: 'https://tiny.cc/n8nsmz',
-			data,
-		});
-	} catch (e) {
-		return res.json({
-			sucess: false,
-			message: e.message,
-		});
-	}
+  try {
+    const data = await scrape.getCases(req.query);
+    return res.json({
+      success: true,
+      source: 'https://tiny.cc/n8nsmz',
+      data,
+    });
+  } catch (e) {
+    return res.json({
+      sucess: false,
+      message: e.message,
+    });
+  }
 });
 
 app.get('/doh-data-drop', async (req, res) => {
-	const { page, itemsPerPage } = req.query;
-
-	try {
-		const data = await scrape.getDOHDataDrop();
-
-		if (page) {
-			if (isNaN(page) || isNaN(itemsPerPage)) {
-				const paginateCollection = paginate(data, 1, 100);
-				return res.json({
-					success: true,
-					source: 'https://tiny.cc/n8nsmz',
-					...paginateCollection,
-				});
-			}
-
-			const paginateCollection = paginate(data, page, itemsPerPage);
-			return res.json({
-				success: true,
-				source: 'https://tiny.cc/n8nsmz',
-				...paginateCollection,
-			});
-		}
-
-		return res.json({
-			success: true,
-			source: 'https://tiny.cc/n8nsmz',
-			data,
-		});
-	} catch (e) {
-		console.log(e);
-		return res.json({
-			sucess: false,
-			message: e.message,
-		});
-	}
+  try {
+    const data = await scrape.getDOHDataDrop(req.query);
+    return res.json({
+      success: true,
+      source: 'https://tiny.cc/n8nsmz',
+      data,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      sucess: false,
+      message: e.message,
+    });
+  }
 });
 
 app.get('/facilities', async (_, res) => {
-	try {
-		const data = await scrape.getFacilities();
-		return res.json({
-			success: true,
-			source: 'https://github.com/gigerbytes/ncov-ph-data',
-			info: 'DOH updated their covid tracker. Data incomplete.',
-			data,
-		});
-	} catch (e) {
-		return res.json({
-			sucess: false,
-			message: e.message,
-		});
-	}
+  try {
+    const data = await scrape.getFacilities();
+    return res.json({
+      success: true,
+      source: 'https://github.com/gigerbytes/ncov-ph-data',
+      info: 'DOH updated their covid tracker. Data incomplete.',
+      data,
+    });
+  } catch (e) {
+    return res.json({
+      sucess: false,
+      message: e.message,
+    });
+  }
 });
 
 // Confirmed cases of Filipino nationals outside the Philippines
 app.get('/cases-outside-ph', async (_, res) => {
-	try {
-		const data = await scrape.getCasesOutsidePh();
-		return res.json({
-			success: true,
-			source:
-				'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_the_Philippines',
-			data,
-		});
-	} catch (e) {
-		return res.json({
-			sucess: false,
-			message: e.message,
-		});
-	}
+  try {
+    const data = await scrape.getCasesOutsidePh();
+    return res.json({
+      success: true,
+      source:
+        'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_the_Philippines',
+      data,
+    });
+  } catch (e) {
+    return res.json({
+      sucess: false,
+      message: e.message,
+    });
+  }
 });
 
 // Metro manila community quarantine checkpoints
 app.get('/mm-checkpoints', async (_, res) => {
-	try {
-		const data = checkpoint.getAll();
-		return res.json({
-			success: true,
-			source: 'https://safetravel.ph',
-			data,
-		});
-	} catch (e) {
-		return res.json({
-			sucess: false,
-			message: e.message,
-		});
-	}
+  try {
+    const data = checkpoint.getAll();
+    return res.json({
+      success: true,
+      source: 'https://safetravel.ph',
+      data,
+    });
+  } catch (e) {
+    return res.json({
+      sucess: false,
+      message: e.message,
+    });
+  }
 });
 
 app.get('/mm-checkpoints/:id', async (req, res) => {
-	try {
-		const data = checkpoint.getOne(req.params.id);
-		return res.json({
-			success: true,
-			source: 'https://safetravel.ph',
-			data,
-		});
-	} catch (e) {
-		return res.json({
-			success: false,
-			message: 'Not found',
-		});
-	}
+  try {
+    const data = checkpoint.getOne(req.params.id);
+    return res.json({
+      success: true,
+      source: 'https://safetravel.ph',
+      data,
+    });
+  } catch (e) {
+    return res.json({
+      success: false,
+      message: 'Not found',
+    });
+  }
 });
 
 module.exports = app;
--- services/Scraper.js
+++ services/Scraper.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const cheerio = require('cheerio');
 const cheerioTableparser = require('cheerio-tableparser');
 const axios = require('axios');
@@ -6,7 +7,7 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 const request = require('request');
 const csv = require('csvtojson');
 const dayjs = require('dayjs');
-require('dotenv').config();
+const paginate = require('paginate-array');
 
 const sheetId = '16g_PUxKYMC0XjeEKF6FPUBq2-pFgmTkHoj5lbVrGLhE';
 const doc = new GoogleSpreadsheet(sheetId);
@@ -15,211 +16,268 @@ const doc = new GoogleSpreadsheet(sheetId);
 doc.useApiKey(process.env.DOC_API_KEY);
 
 class Scraper {
-	getFacilities() {
-		const data = [];
-		return new Promise((resolve, reject) => {
-			csv()
-				.fromStream(
-					request.get(
-						'https://raw.githubusercontent.com/gigerbytes/ncov-ph-data/master/data/facilities.csv'
-					)
-				)
-				.subscribe(
-					(json) => {
-						data.push(json);
-					},
-					function (e) {
-						reject(e);
-					},
-					function () {
-						resolve(
-							data.map((i) => {
-								const clone = { ...i };
-								delete clone.dashboard_last_updated;
-								delete clone.inserted_at;
-								clone.puis = +i.puis;
-								clone.confirmed_cases = +i.confirmed_cases;
-								clone.latitude = +i.latitude;
-								clone.longitude = +i.longitude;
-								return clone;
-							})
-						);
-					}
-				);
-		});
-	}
+  getFacilities() {
+    const data = [];
+    return new Promise((resolve, reject) => {
+      csv()
+        .fromStream(
+          request.get(
+            'https://raw.githubusercontent.com/gigerbytes/ncov-ph-data/master/data/facilities.csv'
+          )
+        )
+        .subscribe(
+          (json) => {
+            data.push(json);
+          },
+          function (e) {
+            reject(e);
+          },
+          function () {
+            resolve(
+              data.map((i) => {
+                const clone = { ...i };
+                delete clone.dashboard_last_updated;
+                delete clone.inserted_at;
+                clone.puis = +i.puis;
+                clone.confirmed_cases = +i.confirmed_cases;
+                clone.latitude = +i.latitude;
+                clone.longitude = +i.longitude;
+                return clone;
+              })
+            );
+          }
+        );
+    });
+  }
 
-	async getSheetByTitle(title) {
-		await doc.loadInfo();
+  async getSheetByTitle(title) {
+    await doc.loadInfo();
 
-		// Find sheet by title
-		const sheetIndex = doc.sheetsByIndex.findIndex((i) => i.title === title);
+    // Find sheet by title
+    const sheetIndex = doc.sheetsByIndex.findIndex((i) => i.title === title);
 
-		if (sheetIndex === -1) {
-			throw {
-				message: 'Sheet not found',
-			};
-		}
+    if (sheetIndex === -1) {
+      throw {
+        message: 'Sheet not found',
+      };
+    }
 
-		const sheet = doc.sheetsByIndex[sheetIndex];
+    const sheet = doc.sheetsByIndex[sheetIndex];
 
-		const rows = await sheet.getRows();
-		return rows;
-	}
+    const rows = await sheet.getRows();
+    return rows;
+  }
 
-	async getTotalCases() {
-		const rows = await this.getSheetByTitle('Historical');
+  async getTotalCases() {
+    const rows = await this.getSheetByTitle('Historical');
 
-		const reversed = rows.reverse();
+    const reversed = rows.reverse();
 
-		let result;
+    let result;
 
-		for (let x = 0; x < reversed.length; x++) {
-			const row = reversed[x];
-			const dateRow = dayjs(row['Date']).format('YYYY-MM-DD');
+    for (let x = 0; x < reversed.length; x++) {
+      const row = reversed[x];
+      const dateRow = dayjs(row['Date']).format('YYYY-MM-DD');
 
-			if (row['Cases']) {
-				result = {
-					last_update: dateRow,
-					cases: +row['Cases'],
-					deaths: +row['Deaths'],
-					recoveries: +row['Recoveries'],
-					cases_today: +row['Daily Case Increase'],
-					deaths_today: +row['Daily Death'],
-					recoveries_today: +row['Daily Recovery'],
-					admitted: +row['Admitted'],
-					fatality_rate: row['Fatality Rate'],
-					recovery_rate: row['Recovery Rate'],
-				};
-				break;
-			}
-		}
+      if (row['Cases']) {
+        result = {
+          last_update: dateRow,
+          cases: +row['Cases'],
+          deaths: +row['Deaths'],
+          recoveries: +row['Recoveries'],
+          cases_today: +row['Daily Case Increase'],
+          deaths_today: +row['Daily Death'],
+          recoveries_today: +row['Daily Recovery'],
+          admitted: +row['Admitted'],
+          fatality_rate: row['Fatality Rate'],
+          recovery_rate: row['Recovery Rate'],
+        };
+        break;
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 
-	async getDOHDataDrop() {
-		const rows = await this.getSheetByTitle('DOH Data Drop');
+  async getDOHDataDrop(query) {
+    const { page, itemsPerPage = 100, filterBy, filterByValue } = query;
+    let rows = (await this.getSheetByTitle('DOH Data Drop')).sort(
+      (a, b) => +new Date(a.DateRepConf) - +new Date(b.DateRepConf)
+    );
 
-		const data = [];
+    // To-Do
+    // Check if filter by is present
+    // if (filterBy && filterByValue) {
+    // 	rows = rows.filter((i) => i[filterBy] && i[filterBy] === filterByValue);
+    // }
 
-		for (let x = 0; x < rows.length; x++) {
-			const row = rows[x];
+    if (page) {
+      const paginateCollection = paginate(rows, page, itemsPerPage);
+      return {
+        ...paginateCollection,
+        data: paginateCollection.data.map((row) => {
+          const formattedRow = {
+            case_code: row['CaseCode'],
+            age: row['Age'] ? +row['Age'] : '',
+            sex: row['Sex'] ? row['Sex'].charAt(0) : '',
+            is_admitted: row['Admitted'],
+            date_reported: dayjs(row['DateRepConf']).format('YYYY-MM-DD'),
+            date_died: row['DateDied']
+              ? dayjs(row['DateDied']).format('YYYY-MM-DD')
+              : '',
+            recovered_on: row['DateRecover']
+              ? dayjs(row['DateRecover']).format('YYYY-MM-DD')
+              : '',
+            region_res: row['RegionRes'],
+            prov_city_res: row['ProvCityRes'],
+            location: row['Location'],
+            latitude: row['Latitude'] ? +row['Latitude'] : '',
+            longitude: row['Longitude'] ? +row['Longitude'] : '',
+          };
 
-			const formattedRow = {
-				case_code: row['CaseCode'],
-				age: row['Age'] ? +row['Age'] : '',
-				sex: row['Sex'] ? row['Sex'].charAt(0) : '',
-				is_admitted: row['Admitted'],
-				date_reported: dayjs(row['DateRepConf']).format('YYYY-MM-DD'),
-				date_died: row['DateDied']
-					? dayjs(row['DateDied']).format('YYYY-MM-DD')
-					: '',
-				recovered_on: row['DateRecover']
-					? dayjs(row['DateRecover']).format('YYYY-MM-DD')
-					: '',
-				region_res: row['RegionRes'],
-				prov_city_res: row['ProvCityRes'],
-				location: row['Location'],
-				latitude: row['Latitude'] ? +row['Latitude'] : '',
-				longitude: row['Longitude'] ? +row['Longitude'] : '',
-			};
+          return formattedRow;
+        }),
+      };
+    }
 
-			data.push(formattedRow);
-		}
+    // // Returns all data
+    // // This is heavy
+    return rows.map((row) => {
+      return {
+        case_code: row['CaseCode'],
+        age: row['Age'] ? +row['Age'] : '',
+        sex: row['Sex'] ? row['Sex'].charAt(0) : '',
+        is_admitted: row['Admitted'],
+        date_reported: dayjs(row['DateRepConf']).format('YYYY-MM-DD'),
+        date_died: row['DateDied']
+          ? dayjs(row['DateDied']).format('YYYY-MM-DD')
+          : '',
+        recovered_on: row['DateRecover']
+          ? dayjs(row['DateRecover']).format('YYYY-MM-DD')
+          : '',
+        region_res: row['RegionRes'],
+        prov_city_res: row['ProvCityRes'],
+        location: row['Location'],
+        latitude: row['Latitude'] ? +row['Latitude'] : '',
+        longitude: row['Longitude'] ? +row['Longitude'] : '',
+      };
+    });
+  }
 
-		return data;
-	}
+  async getCases(query) {
+    const { page, itemsPerPage = 100 } = query;
+    let rows = (await this.getSheetByTitle('Cases')).sort(
+      (a, b) =>
+        +new Date(a['Date of Announcement to the Public']) -
+        +new Date(b['Date of Announcement to the Public'])
+    );
 
-	async getCases() {
-		const rows = await this.getSheetByTitle('Cases');
+    if (page) {
+      const paginateCollection = paginate(rows, page, itemsPerPage);
+      return {
+        ...paginateCollection,
+        data: paginateCollection.data.map((row) => {
+          return {
+            case_no: row['Case No.'],
+            sex: row['Sex'] ? row['Sex'].charAt(0) : '',
+            age: row['Age'] ? +row['Age'] : '',
+            nationality: row['Nationality'],
+            residence_in_the_ph: row['Residence in the Philippines'],
+            travel_history: row['Travel History'],
+            date_of_announcement_to_public: dayjs(
+              row['Date of Announcement to the Public']
+            ).format('YYYY-MM-DD'),
+            hospital_admitted_to: row['Admission / Consultation'],
+            status: row['Status'],
+            health_status: row['Health Status'],
+            location: row['Location'],
+            latitude: row['Latitude'] ? +row['Latitude'] : '',
+            longitude: row['Longitude'] ? +row['Longitude'] : '',
+            residence_lat: row['Residence Lat'] ? +row['Residence Lat'] : '',
+            residence_long: row['Residence Long'] ? +row['Residence Long'] : '',
+          };
+        }),
+      };
+    }
 
-		const data = [];
+    return rows.map((row) => {
+      return {
+        case_no: row['Case No.'],
+        sex: row['Sex'] ? row['Sex'].charAt(0) : '',
+        age: row['Age'] ? +row['Age'] : '',
+        nationality: row['Nationality'],
+        residence_in_the_ph: row['Residence in the Philippines'],
+        travel_history: row['Travel History'],
+        date_of_announcement_to_public: dayjs(
+          row['Date of Announcement to the Public']
+        ).format('YYYY-MM-DD'),
+        hospital_admitted_to: row['Admission / Consultation'],
+        status: row['Status'],
+        health_status: row['Health Status'],
+        location: row['Location'],
+        latitude: row['Latitude'] ? +row['Latitude'] : '',
+        longitude: row['Longitude'] ? +row['Longitude'] : '',
+        residence_lat: row['Residence Lat'] ? +row['Residence Lat'] : '',
+        residence_long: row['Residence Long'] ? +row['Residence Long'] : '',
+      };
+    });
+  }
 
-		for (let x = 0; x < rows.length; x++) {
-			const row = rows[x];
+  async getCasesOutsidePh() {
+    const res = await axios(
+      'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_the_Philippines'
+    );
+    const $ = cheerio.load(res.data);
+    cheerioTableparser($);
+    const rawData = $('.wikitable').eq(1).parsetable(true, true, true);
 
-			const formattedRow = {
-				case_no: row['Case No.'],
-				sex: row['Sex'] ? row['Sex'].charAt(0) : '',
-				age: row['Age'] ? +row['Age'] : '',
-				nationality: row['Nationality'],
-				residence_in_the_ph: row['Residence in the Philippines'],
-				travel_history: row['Travel History'],
-				date_of_announcement_to_public: dayjs(
-					row['Date of Announcement to the Public']
-				).format('YYYY-MM-DD'),
-				hospital_admitted_to: row['Admission / Consultation'],
-				status: row['Status'],
-				health_status: row['Health Status'],
-				location: row['Location'],
-				latitude: row['Latitude'] ? +row['Latitude'] : '',
-				longitude: row['Longitude'] ? +row['Longitude'] : '',
-				residence_lat: row['Residence Lat'] ? +row['Residence Lat'] : '',
-				residence_long: row['Residence Long'] ? +row['Residence Long'] : '',
-			};
+    const formattedData = [];
 
-			data.push(formattedRow);
-		}
+    rawData[0].forEach((item, idx) => {
+      const skip = [
+        0,
+        rawData[0].length - 1,
+        rawData[0].length - 2,
+        rawData[0].length - 3,
+      ];
+      if (skip.includes(idx)) return;
 
-		return data;
-	}
+      const obj = {
+        country_territory_place: item.split('[')[0],
+        confirmed: +rawData[1][idx].split('[')[0],
+        recovered: +rawData[2][idx].split('[')[0],
+        died: +rawData[3][idx].split('[')[0],
+      };
 
-	async getCasesOutsidePh() {
-		const res = await axios(
-			'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_the_Philippines'
-		);
-		const $ = cheerio.load(res.data);
-		cheerioTableparser($);
-		const rawData = $('.wikitable').eq(1).parsetable(true, true, true);
+      formattedData.push(obj);
+    });
 
-		const formattedData = [];
+    return formattedData;
+  }
 
-		rawData[0].forEach((item, idx) => {
-			const skip = [
-				0,
-				rawData[0].length - 1,
-				rawData[0].length - 2,
-				rawData[0].length - 3,
-			];
-			if (skip.includes(idx)) return;
+  async getLaboratoryStatusOfPatients() {
+    const agent = new https.Agent({
+      rejectUnauthorized: false,
+    });
+    const res = await axios('https://www.doh.gov.ph/2019-nCov', {
+      httpsAgent: agent,
+    });
+    const $ = cheerio.load(res.data);
 
-			const obj = {
-				country_territory_place: item.split('[')[0],
-				confirmed: +rawData[1][idx].split('[')[0],
-				recovered: +rawData[2][idx].split('[')[0],
-				died: +rawData[3][idx].split('[')[0],
-			};
+    const formattedData = {};
 
-			formattedData.push(obj);
-		});
+    $('table')
+      .eq(0)
+      .find('tbody tr')
+      .each((idx, el) => {
+        const td = $(el).children();
+        formattedData[
+          td.eq(0).text().trim().split(' ').join('_').toLowerCase()
+        ] = +td.eq(1).text().trim().split('(')[0].replace(/\,/g, '');
+      });
 
-		return formattedData;
-	}
-
-	async getLaboratoryStatusOfPatients() {
-		const agent = new https.Agent({
-			rejectUnauthorized: false,
-		});
-		const res = await axios('https://www.doh.gov.ph/2019-nCov', {
-			httpsAgent: agent,
-		});
-		const $ = cheerio.load(res.data);
-
-		const formattedData = {};
-
-		$('table')
-			.eq(0)
-			.find('tbody tr')
-			.each((idx, el) => {
-				const td = $(el).children();
-				formattedData[
-					td.eq(0).text().trim().split(' ').join('_').toLowerCase()
-				] = +td.eq(1).text().trim().split('(')[0].replace(/\,/g, '');
-			});
-
-		return formattedData;
-	}
+    return formattedData;
+  }
 }
 
 module.exports = Scraper;
